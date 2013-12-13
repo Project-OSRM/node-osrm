ffe8be3f439a22a1d16f61cd64242141495bea69
John Firebaugh <john.firebaugh@gmail.com>
2013-12-13 13:55:45 -0500

    Early return

--- src/query.hpp
+++ src/query.hpp
@@ -54,57 +54,56 @@ Handle<Value> Query::New(Arguments const& args)
         return ThrowException(Exception::Error(String::New("Cannot call constructor as function, you need to use 'new' keyword")));
     }
     try {
-        if (args.Length() == 1) {
-            if (!args[0]->IsObject()) {
-                return ThrowException(Exception::TypeError(String::New("first argument must be an object")));
-            }
-            Local<Object> obj = args[0]->ToObject();
-            if (obj->IsNull() || obj->IsUndefined()) {
-                return ThrowException(Exception::TypeError(String::New("first arg must be an object")));
-            }
-            if (!obj->Has(String::NewSymbol("coordinates"))) {
-                return ThrowException(Exception::TypeError(String::New("must provide a coordinates property")));
-            }
-            Local<Value> coordinates = obj->Get(String::New("coordinates"));
-            if (!coordinates->IsArray()) {
-                return ThrowException(Exception::TypeError(String::New("coordinates must be an array of (lat/long) pairs")));
-            }
-            Local<Array> coordinates_array = Local<Array>::Cast(coordinates);
-            if (coordinates_array->Length() < 2) {
-                return ThrowException(Exception::TypeError(String::New("at least two coordinates must be provided")));
-            }
-
-            Query* q = new Query();
-            q->this_->zoomLevel = 18; //no generalization
-            q->this_->printInstructions = true; //turn by turn instructions
-            q->this_->alternateRoute = true; //get an alternate route, too
-            q->this_->geometry = true; //retrieve geometry of route
-            q->this_->compression = true; //polyline encoding
-            q->this_->checkSum = UINT_MAX; //see wiki
-            q->this_->service = "viaroute"; //that's routing
-            q->this_->outputFormat = "json";
-            q->this_->jsonpParameter = ""; //set for jsonp wrapping
-            q->this_->language = ""; //unused atm
-
-            for (uint32_t i = 0; i < coordinates_array->Length(); ++i) {
-                Local<Value> coordinate = coordinates_array->Get(i);
-                if (!coordinate->IsArray()) {
-                    return ThrowException(Exception::TypeError(String::New("coordinates must be an array of (lat/long) pairs")));
-                }
-                Local<Array> coordinate_array = Local<Array>::Cast(coordinate);
-                if (coordinate_array->Length() != 2) {
-                    return ThrowException(Exception::TypeError(String::New("coordinates must be an array of (lat/long) pairs")));
-                }
-                q->this_->coordinates.push_back(
-                    FixedPointCoordinate(coordinate_array->Get(0)->NumberValue()*COORDINATE_PRECISION,
-                                         coordinate_array->Get(1)->NumberValue()*COORDINATE_PRECISION));
-            }
-
-            q->Wrap(args.This());
-            return args.This();
-        } else {
+        if (args.Length() != 1) {
             return ThrowException(Exception::TypeError(String::New("please provide an object of options for the first argument")));
         }
+        if (!args[0]->IsObject()) {
+            return ThrowException(Exception::TypeError(String::New("first argument must be an object")));
+        }
+        Local<Object> obj = args[0]->ToObject();
+        if (obj->IsNull() || obj->IsUndefined()) {
+            return ThrowException(Exception::TypeError(String::New("first arg must be an object")));
+        }
+        if (!obj->Has(String::NewSymbol("coordinates"))) {
+            return ThrowException(Exception::TypeError(String::New("must provide a coordinates property")));
+        }
+        Local<Value> coordinates = obj->Get(String::New("coordinates"));
+        if (!coordinates->IsArray()) {
+            return ThrowException(Exception::TypeError(String::New("coordinates must be an array of (lat/long) pairs")));
+        }
+        Local<Array> coordinates_array = Local<Array>::Cast(coordinates);
+        if (coordinates_array->Length() < 2) {
+            return ThrowException(Exception::TypeError(String::New("at least two coordinates must be provided")));
+        }
+
+        Query* q = new Query();
+        q->this_->zoomLevel = 18; //no generalization
+        q->this_->printInstructions = true; //turn by turn instructions
+        q->this_->alternateRoute = true; //get an alternate route, too
+        q->this_->geometry = true; //retrieve geometry of route
+        q->this_->compression = true; //polyline encoding
+        q->this_->checkSum = UINT_MAX; //see wiki
+        q->this_->service = "viaroute"; //that's routing
+        q->this_->outputFormat = "json";
+        q->this_->jsonpParameter = ""; //set for jsonp wrapping
+        q->this_->language = ""; //unused atm
+
+        for (uint32_t i = 0; i < coordinates_array->Length(); ++i) {
+            Local<Value> coordinate = coordinates_array->Get(i);
+            if (!coordinate->IsArray()) {
+                return ThrowException(Exception::TypeError(String::New("coordinates must be an array of (lat/long) pairs")));
+            }
+            Local<Array> coordinate_array = Local<Array>::Cast(coordinate);
+            if (coordinate_array->Length() != 2) {
+                return ThrowException(Exception::TypeError(String::New("coordinates must be an array of (lat/long) pairs")));
+            }
+            q->this_->coordinates.push_back(
+                FixedPointCoordinate(coordinate_array->Get(0)->NumberValue()*COORDINATE_PRECISION,
+                                     coordinate_array->Get(1)->NumberValue()*COORDINATE_PRECISION));
+        }
+
+        q->Wrap(args.This());
+        return args.This();
     } catch (std::exception const& ex) {
         return ThrowException(Exception::TypeError(String::New(ex.what())));
     }
