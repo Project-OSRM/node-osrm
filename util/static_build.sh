911d3694cf73cc409ea9196f147787c5f1137887
Dane Springmeyer <dane@dbsgeo.com>
2014-01-15 10:16:13 -0800

    [publish-binary]

--- util/static_build.sh
+++ util/static_build.sh
@@ -10,5 +10,8 @@ UNAME=$(uname -s);
 if [ ${UNAME} = 'Darwin' ]; then
     build_osrm_for_osx
 else
+    # fixes node/gyp install
+    # https://github.com/travis-ci/travis-cookbooks/issues/155
+    sudo rm -rf /dev/shm && sudo ln -s /run/shm /dev/shm
     build_osrm_for_linux
 fi
