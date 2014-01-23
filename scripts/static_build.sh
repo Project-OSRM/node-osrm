ee896590c1fb59f3e0ca2947d9ab604be49ae067
Dane Springmeyer <dane@dbsgeo.com>
2014-01-23 12:43:22 -0800

    static build fixes: adapt to upstream changes + be able to run locally

--- scripts/static_build.sh
+++ scripts/static_build.sh
@@ -1,24 +1,31 @@
 #!/bin/bash
 
+set -u
+if [[ ${TMP_DEPS_DIR:-false} == false ]]; then
+    TMP_DEPS_DIR=/tmp/osrm-build
+    rm -rf ${TMP_DEPS_DIR}
+    mkdir -p ${TMP_DEPS_DIR}
+fi
+if [[ ${NODE_MODULE_ROOT:-false} == false ]]; then
+    NODE_MODULE_ROOT=`pwd`
+fi
 git clone --depth=0 https://github.com/mapnik/mapnik-packaging.git  ${TMP_DEPS_DIR}/mapnik-packaging
 cd ${TMP_DEPS_DIR}/mapnik-packaging
 export CXX11=false
 source build.sh
 UNAME=$(uname -s);
-if [ ${UNAME} = 'Darwin' ]; then
-    build_osrm_for_osx
-else
+if [ ${UNAME} = 'Linux' ]; then
     # fixes node/gyp install
     # https://github.com/travis-ci/travis-cookbooks/issues/155
     sudo rm -rf /dev/shm && sudo ln -s /run/shm /dev/shm
-    build_osrm_for_linux
 fi
-if [[ ${PACKAGE_COMMAND_LINE_TOOLS} == true ]]; then
+build_osrm
+if [[ ${PACKAGE_COMMAND_LINE_TOOLS:-false} != false ]]; then
     mkdir -p "${NODE_MODULE_ROOT}/lib/binding/"
     cp `which lua` "${NODE_MODULE_ROOT}/lib/binding/"
     cp `which osrm-extract` "${NODE_MODULE_ROOT}/lib/binding/"
     cp `which osrm-prepare` "${NODE_MODULE_ROOT}/lib/binding/"
     cp `which osrm-datastore` "${NODE_MODULE_ROOT}/lib/binding/"
 else
-    echo "node packaging command line tools"
+    echo "not packaging command line tools"
 fi
