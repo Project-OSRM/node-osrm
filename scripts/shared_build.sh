d99605e3a430cb7b5bb905c2f83d4095f0934f16
Dane Springmeyer <dane@mapbox.com>
2014-05-01 12:48:34 -0700

    cleanup shared build script [skip ci]

--- scripts/shared_build.sh
+++ scripts/shared_build.sh
@@ -2,23 +2,25 @@
 
 UNAME=$(uname -s);
 if [ ${UNAME} = 'Darwin' ]; then
-    brew install boost cmake protobuf libstxxl lua luabind osm-pbf
+    brew install boost cmake protobuf libstxxl luaosm-pbf
+    DEPS_PREFIX=$(brew --prefix)
+    # install luabind
+    git clone --depth=1 https://github.com/DennisOSRM/luabind.git
+    cd luabind
+    mkdir -p build
+    cd build
+    cmake ../ -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${DEPS_PREFIX}
+    make
+    make install
+    cd ../../
+
 else
     # install packages
     sudo apt-add-repository --yes ppa:mapnik/boost # boost 1.49 (in future 1.55)
     sudo apt-get -qq update
     sudo apt-get install -y libboost-filesystem-dev libboost-program-options-dev libboost-iostreams-dev libboost-regex-dev libboost-system-dev libboost-thread-dev
     sudo apt-get install -y build-essential git cmake libprotoc-dev libprotobuf7 protobuf-compiler libprotobuf-dev libbz2-dev libstxxl-dev libstxxl-doc libstxxl1 libxml2-dev libzip-dev lua5.1 liblua5.1-0-dev
-
-    # install luabind
-    git clone --depth=1 https://github.com/DennisOSRM/luabind.git
-    cd luabind
-    mkdir -p build
-    cd build
-    cmake ../ -DCMAKE_BUILD_TYPE=Release
-    make
-    sudo make install
-    cd ../../
+    DEPS_PREFIX="/usr/local"
 
     # install osmpbf
     git clone --depth=1 https://github.com/scrosby/OSM-binary.git
@@ -32,10 +34,14 @@ fi
 # install OSRM
 git clone --depth=1 https://github.com/DennisOSRM/Project-OSRM.git Project-OSRM -b develop
 cd Project-OSRM
-mkdir -p build 
+mkdir -p build
 cd build
-cmake ../ -DWITH_TOOLS=1
-make
+cmake ../ \
+  -DCMAKE_INCLUDE_PATH=${DEPS_PREFIX}/include \
+  -DCMAKE_LIBRARY_PATH=${DEPS_PREFIX}/lib \
+  -DCMAKE_BUILD_TYPE=Release
+
+make VERBOSE=1
 sudo make install
 cd ../../
 pkg-config libosrm --cflags
