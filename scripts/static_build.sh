d50b4f6e073741064946966dc9145d5b2d47c95b
Dane Springmeyer <dane@mapbox.com>
2015-01-06 12:02:32 -0800

    explicitly install pkg-config

--- scripts/static_build.sh
+++ scripts/static_build.sh
@@ -6,6 +6,12 @@ TMP_DEPS_DIR=`mktemp -d -t XXXXXXXXXXX`;
 if [[ ${NODE_MODULE_ROOT:-false} == false ]]; then
     NODE_MODULE_ROOT=`pwd`
 fi
+
+if [[ ! $(which pkg-config) ]]; then
+    echo "missing pkg-config"
+    exit 1
+fi
+
 git clone --depth=1 https://github.com/mapnik/mapnik-packaging.git  ${TMP_DEPS_DIR}/mapnik-packaging
 cd ${TMP_DEPS_DIR}/mapnik-packaging
 export CXX11=true
