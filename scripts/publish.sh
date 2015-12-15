dca03d705a22d683f3d8be0016765ebdcb0f0d07
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2015-12-15 18:10:21 +0100

    Move to cmake-js

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,6 +1,3 @@
-
-./node_modules/.bin/node-pre-gyp package ${NPM_FLAGS}
-
 COMMIT_MESSAGE=$(git show -s --format=%B $TRAVIS_COMMIT | tr -d '\n')
 
 if [[ ${COMMIT_MESSAGE} =~ "[publish binary]" ]]; then
