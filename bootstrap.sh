a4b9c861126eca5b7276bd53819511db0fcd4cd8
Patrick Niklaus <patrick.niklaus@student.kit.edu>
2015-12-21 21:25:10 +0100

    Change release name to track develop

--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@ CURRENT_DIR=$(pwd)
 # default to clang
 CXX=${CXX:-clang++}
 TARGET=${TARGET:-Release}
-OSRM_RELEASE=${OSRM_RELEASE:-"v4.8.1"}
+OSRM_RELEASE=${OSRM_RELEASE:-"develop"}
 OSRM_REPO=${OSRM_REPO:-"https://github.com/Project-OSRM/osrm-backend.git"}
 
 function all_deps() {
