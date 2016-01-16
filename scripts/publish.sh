57ef2ff578f2512c63a702ad8cb20fcdc087fe4f
Dane Springmeyer <dane@mapbox.com>
2016-01-16 11:05:32 -0800

    fix [republish binary] via pull request - refs mapbox/node-pre-gyp#196

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,7 +1,7 @@
 
 ./node_modules/.bin/node-pre-gyp package ${NPM_FLAGS}
 
-COMMIT_MESSAGE=$(git show -s --format=%B $TRAVIS_COMMIT | tr -d '\n')
+COMMIT_MESSAGE=$(git log --format=%B --no-merges -n 1 | tr -d '\n')
 echo ${COMMIT_MESSAGE}
 
 if [[ ${COMMIT_MESSAGE} =~ "[publish binary]" ]]; then
