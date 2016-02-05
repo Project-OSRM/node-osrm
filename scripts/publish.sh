c9c45f2c04924b736ed17cca1d376d4fb31c1956
Freenerd <nospam@freenerd.de>
2016-02-05 13:56:49 +0100

    Fix publishing on PRs

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,10 +1,16 @@
+#!/bin/bash
 
 ./node_modules/.bin/node-pre-gyp package ${NPM_FLAGS}
 
-COMMIT_MESSAGE=$(git show -s --format=%B $TRAVIS_COMMIT | tr -d '\n')
+COMMIT_MESSAGE=$(git log --format=%B --no-merges | head -n 1 | tr -d '\n')
+echo "Commit message: ${COMMIT_MESSAGE}"
 
 if [[ ${COMMIT_MESSAGE} =~ "[publish binary]" ]]; then
+    echo "Publishing"
     ./node_modules/.bin/node-pre-gyp publish ${NPM_FLAGS}
 elif [[ ${COMMIT_MESSAGE} =~ "[republish binary]" ]]; then
+    echo "Re-Publishing"
     ./node_modules/.bin/node-pre-gyp unpublish publish ${NPM_FLAGS}
+else
+    echo "Skipping publishing"
 fi;
