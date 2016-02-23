d5f06217312c7ed62289e06f3baf00ff30057c07
Lauren Budorick <lauren@mapbox.com>
2016-02-23 14:37:01 -0800

    all 5 services :boom:

--- test/nearest.js
+++ test/nearest.js
@@ -3,12 +3,27 @@ var test = require('tape');
 var berlin_path = require('./osrm-data-path').data_path;
 
 test('nearest', function(assert) {
-    assert.plan(3);
+    assert.plan(4);
     var osrm = new OSRM(berlin_path);
-    osrm.nearest([52.4224, 13.333086], function(err, result) {
+    osrm.nearest({
+        coordinates: [[52.4224, 13.333086]]
+    }, function(err, result) {
         assert.ifError(err);
-        assert.equal(result.mapped_coordinate.length, 2);
-        assert.ok(result.hasOwnProperty('name'));
+        assert.equal(result.waypoints.length, 1);
+        assert.equal(result.waypoints[0].location.length, 2);
+        assert.ok(result.waypoints[0].hasOwnProperty('name'));
+    });
+});
+
+test('nearest: can ask for multiple nearest pts', function(assert) {
+    assert.plan(2);
+    var osrm = new OSRM(berlin_path);
+    osrm.nearest({
+        coordinates: [[52.4224, 13.333086]],
+        number: 3
+    }, function(err, result) {
+        assert.ifError(err);
+        assert.equal(result.waypoints.length, 3);
     });
 });
 
@@ -19,8 +34,8 @@ test('nearest: throws on invalid args', function(assert) {
     assert.throws(function() { osrm.nearest(options); },
         /two arguments required/);
     assert.throws(function() { osrm.nearest(null, function(err, res) {}); },
-        /first argument must be an array of lat, long/);
+        /first arg must be an object/);
     options.coordinates = [52.4224];
     assert.throws(function() { osrm.nearest(options, function(err, res) {}); },
-        /first argument must be an array of lat, long/);
+        /coordinates must be an array of /);
 });
