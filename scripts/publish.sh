2f3422013ba2b4dbf90fdd01d4725c15c41f81d1
Dane Springmeyer <dane@mapbox.com>
2016-02-29 19:30:36 -0800

    no usage of NPM_FLAGS anymore [skip ci]

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-./node_modules/.bin/node-pre-gyp package ${NPM_FLAGS}
+./node_modules/.bin/node-pre-gyp package
 
 COMMIT_MESSAGE=$(git log --format=%B --no-merges | head -n 1 | tr -d '\n')
 echo "Commit message: ${COMMIT_MESSAGE}"
 
 if [[ ${COMMIT_MESSAGE} =~ "[publish binary]" ]]; then
     echo "Publishing"
-    ./node_modules/.bin/node-pre-gyp publish ${NPM_FLAGS}
+    ./node_modules/.bin/node-pre-gyp publish
 elif [[ ${COMMIT_MESSAGE} =~ "[republish binary]" ]]; then
     echo "Re-Publishing"
-    ./node_modules/.bin/node-pre-gyp unpublish publish ${NPM_FLAGS}
+    ./node_modules/.bin/node-pre-gyp unpublish publish
 else
     echo "Skipping publishing"
 fi;
