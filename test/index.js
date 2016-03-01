f02d38fc552d1c6b6ba3dbef5a20aa9a8c06e9ad
Dane Springmeyer <dane@mapbox.com>
2016-02-29 18:06:19 -0800

    avoid hardcoding path to berlin-latest.osrm in each test

--- test/index.js
+++ test/index.js
@@ -1,6 +1,6 @@
 var OSRM = require('../');
 var test = require('tape');
-var berlin_path = "test/data/berlin-latest.osrm";
+var berlin_path = require('./osrm-data-path').data_path;
 
 test('constructor: throws if new keyword is not used', function(assert) {
     assert.plan(1);
